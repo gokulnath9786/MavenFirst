7cf0fa026d6bcf377e6a30005b9127695e888136
gokulnath9786 <gokulnath9786@gmail.com>
2022-03-19 21:42:29 +0530

    First program

---
+++ src/test/java/org/testngsample/TestngSample.java
@@ -0,0 +1,12 @@
+package org.testngsample;
+
+import org.testng.annotations.Test;
+
+public class TestngSample {
+@Test
+private void test1() {
+	// TODO Auto-generated method stub
+	System.out.println("to print");
+System.out.println("test1");
+}
+}
---
+++ src/test/java/org/testngsample/test.java
@@ -0,0 +1,5 @@
+package org.testngsample;
+
+public @interface test {
+
+}
